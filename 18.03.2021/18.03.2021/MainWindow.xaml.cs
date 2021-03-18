2d2370f10cd7d036e01fc8ebb06eeff6a6e21b90
Wedyarit <kitchenman4@yandex.ru>
2021-03-18 17:39:17 +0600

    18.03.2021

---
+++ 18.03.2021/18.03.2021/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _18._03._2021
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            EventManager.RegisterClassHandler(typeof(Button), Button.ClickEvent, new RoutedEventHandler(Calculate));
+
+        }
+
+        private void Calculate(object sender, RoutedEventArgs e) {
+            MessageBox.Show("Ok, You click me.", "Answer");
+        }
+    }
+}
