1aee13c62212496c1f8810765cb367e01b0a0425
Wedyarit <kitchenman4@yandex.ru>
2021-03-29 13:53:05 +0600

    Reinit.

---
+++ 26.03.2021/CW/28.03.2021/28.03.2021/FB2Reader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Documents;
+using System.Xml.Linq;
+
+namespace _28._03._2021
+{
+    class FB2Reader
+    {
+        private string _filename;
+        private XDocument _xDocument;
+        private List<Block> _blocks;
+
+        public FB2Reader(string _filename)
+        {
+            string content = File.ReadAllText(_filename);
+            if (content.Length == 0) { MessageBox.Show("File is empty!"); return; }
+            else _xDocument = XDocument.Parse(content);
+        }
+
+        public List<Block> GetBlocks()
+        {
+            _blocks = new List<Block>();
+            foreach (XElement xElement in _xDocument.Descendants())
+            {
+                Trace.WriteLine(xElement.Name);
+                _blocks.Add(new Paragraph(new Run(xElement.Value)));
+            }
+
+            return _blocks;
+        }
+    }
+}
---
+++ 26.03.2021/CW/28.03.2021/28.03.2021/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+
+namespace _28._03._2021
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private FB2Reader _FB2Reader;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void miClickExit(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+        private void miClickOpen(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "FictionBook (*.fb2)|*.fb2";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _FB2Reader = new FB2Reader(openFileDialog.FileName);
+                foreach (Block block in _FB2Reader.GetBlocks())
+                    fdContent.Blocks.Add(block);
+            }
+        }
+    }
+}
