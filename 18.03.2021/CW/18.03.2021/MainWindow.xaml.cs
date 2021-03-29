bd79abc7d2f85541394b8b490e3039456f5b1b37
Wedyarit <kitchenman4@yandex.ru>
2021-03-29 17:36:45 +0600

    CW 29.03.2021

--- 18.03.2021/CW/18.03.2021/MainWindow.xaml.cs
+++ 18.03.2021/CW/18.03.2021/MainWindow.xaml.cs
@@ -20,15 +20,95 @@ namespace _18._03._2021
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Calculator _calculator;
+
+        char KeyToChar(Key key)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftAlt) ||
+                Keyboard.IsKeyDown(Key.RightAlt) ||
+                Keyboard.IsKeyDown(Key.LeftCtrl) ||
+                Keyboard.IsKeyDown(Key.RightAlt))
+            {
+                return '\x00';
+            }
+
+            switch (key)
+            {
+                case Key.Enter: return '\n';
+                case Key.D0: return (')');
+                case Key.D1: return ('1');
+                case Key.D2: return ('2');
+                case Key.D3: return ('3');
+                case Key.D4: return ('4');
+                case Key.D5: return ('%');
+                case Key.D6: return ('6');
+                case Key.D7: return ('7');
+                case Key.D8: return ('8');
+                case Key.D9: return ('(');
+                case Key.OemPlus: return ('+');
+                case Key.OemMinus: return ('-');
+                case Key.OemQuestion: return ('/');
+                case Key.OemPeriod: return ('.');
+
+                case Key.NumPad0: return '0';
+                case Key.NumPad1: return '1';
+                case Key.NumPad2: return '2';
+                case Key.NumPad3: return '3';
+                case Key.NumPad4: return '4';
+                case Key.NumPad5: return '5';
+                case Key.NumPad6: return '6';
+                case Key.NumPad7: return '7';
+                case Key.NumPad8: return '8';
+                case Key.NumPad9: return '9';
+                case Key.Subtract: return '-';
+                case Key.Add: return '+';
+                case Key.Decimal: return '.';
+                case Key.Divide: return '/';
+                case Key.Multiply: return '*';
+
+                default: return '\x00';
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            _calculator = new Calculator();
+            this.DataContext = _calculator;
             EventManager.RegisterClassHandler(typeof(Button), Button.ClickEvent, new RoutedEventHandler(Calculate));
-
+            this.KeyDown += new KeyEventHandler(KeyCalculate);
         }
 
         private void Calculate(object sender, RoutedEventArgs e) {
-            MessageBox.Show("Ok, You click me.", "Answer");
+            _calculator.Calculate((sender as Button).Content as string);
+        }
+
+        private void KeyCalculate(object sender, KeyEventArgs e)
+        {
+            _calculator.Calculate(KeyToChar(e.Key).ToString());
+        }
+    }
+
+    class Calculator
+    {
+        public string CurrentNumber { get; set; }
+        public string History { get; set; }
+
+        public Calculator()
+        {
+            CurrentNumber = "0";
+            History = "";
+        }
+
+        public void Calculate(string content)
+        {
+            if (Int32.TryParse(content, out int n) || content == ".")
+            {
+                if (CurrentNumber == "0")
+                    CurrentNumber = n.ToString();
+                else
+                    CurrentNumber += content;
+            }
         }
     }
 }
---
+++ 29.03.2021/29.03.2021/29.03.2021/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace _29._03._2021
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PagingCollectionView _cview;
+
+        private void InitializePagingCollectionView()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(File.ReadAllText("MOCK_DATA.xml"));
+            XmlNodeList nodes = xmlDoc.GetElementsByTagName("record");
+            List<User> users = new List<User>();
+            foreach (XmlNode node in nodes)
+                users.Add(new User()
+                {
+                    ID = node.ChildNodes[0].InnerText,
+                    FirstName = node.ChildNodes[1].InnerText,
+                    LastName = node.ChildNodes[2].InnerText,
+                    Email = node.ChildNodes[3].InnerText,
+                    IP = node.ChildNodes[4].InnerText
+                });
+            _cview = new PagingCollectionView(users, 12);
+            DataContext = _cview;
+        }
+
+        private void OnNextClicked(object sender, RoutedEventArgs e) => _cview.MoveToNextPage();
+
+        private void OnPreviousClicked(object sender, RoutedEventArgs e) => _cview.MoveToPreviousPage();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializePagingCollectionView();
+        }
+    }
+
+
+    class User
+    {
+        public string ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string IP { get; set; }
+    }
+}
