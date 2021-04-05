c1f83447b5ada9328503fafd0889694909bba753
Wedyarit <kitchenman4@yandex.ru>
2021-04-05 17:51:16 +0600

    Feat: Updated pictures

---
+++ Messenger/Messenger/Messenger/MessageBox.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Messenger
+{
+    /// <summary>
+    /// Логика взаимодействия для Messenger.xaml
+    /// </summary>
+    public partial class MessageBox : UserControl
+    {
+        public string Author
+        {
+            get { return (string)GetValue(AuthorProperty); }
+            set { SetValue(AuthorProperty, value); }
+        }
+        public static readonly DependencyProperty AuthorProperty =
+            DependencyProperty.Register("Author", typeof(string),
+            typeof(MessageBox), new UIPropertyMetadata("", new PropertyChangedCallback(AuthorChanged)));
+
+        private static void AuthorChanged(DependencyObject depObj, DependencyPropertyChangedEventArgs args)
+        {
+            ((MessageBox)depObj).tbAuthor.Text = $" {args.NewValue.ToString()}";
+        }
+
+        public new string Content
+        {
+            get { return (string)GetValue(ContentProperty); }
+            set { SetValue(ContentProperty, value); }
+        }
+        public static new readonly DependencyProperty ContentProperty =  DependencyProperty.Register("Content", typeof(string),
+            typeof(MessageBox), new UIPropertyMetadata("", new PropertyChangedCallback(ContentChanged)));
+        private static void ContentChanged(DependencyObject depObj, DependencyPropertyChangedEventArgs args)
+        {
+            ((MessageBox)depObj).tbContent.Text = $" {args.NewValue.ToString()}";
+        }
+
+        public string Time
+        {
+            get { return (string)GetValue(TimeProperty); }
+            set { SetValue(TimeProperty, value); }
+        }
+        public static readonly DependencyProperty TimeProperty =
+            DependencyProperty.Register("Time", typeof(string),
+            typeof(MessageBox), new UIPropertyMetadata("", new PropertyChangedCallback(TimeChanged)));
+        private static void TimeChanged(DependencyObject depObj, DependencyPropertyChangedEventArgs args)
+        {
+            ((MessageBox)depObj).tbTime.Text = args.NewValue.ToString();
+        }
+
+        public bool Mine
+        {
+            get { return (bool)GetValue(MineProperty); }
+            set { SetValue(MineProperty, value); }
+        }
+        public static readonly DependencyProperty MineProperty =
+            DependencyProperty.Register("Mine", typeof(bool),
+            typeof(MessageBox), new UIPropertyMetadata(false, new PropertyChangedCallback(MineChanged)));
+        private static void MineChanged(DependencyObject depObj, DependencyPropertyChangedEventArgs args)
+        {
+            if ((bool)args.NewValue)
+            {
+                ((MessageBox)depObj).tbContent.Style = (Style)((MessageBox)depObj).FindResource("MessageContentMe");
+                ((MessageBox)depObj).spMain.Style = (Style)((MessageBox)depObj).FindResource("MessageMe");
+                ((MessageBox)depObj).bBorder.Style = (Style)((MessageBox)depObj).FindResource("MessageBorderMe");
+                ((MessageBox)depObj).spTooltip.HorizontalAlignment = HorizontalAlignment.Left;
+            } else
+            {
+                ((MessageBox)depObj).tbContent.Style = (Style)((MessageBox)depObj).FindResource("MessageContentAnother");
+                ((MessageBox)depObj).spMain.Style = (Style)((MessageBox)depObj).FindResource("MessageAnother");
+                ((MessageBox)depObj).bBorder.Style = (Style)((MessageBox)depObj).FindResource("MessageBorderAnother");
+                ((MessageBox)depObj).spTooltip.HorizontalAlignment = HorizontalAlignment.Right;
+            }
+        }
+        public MessageBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Messenger/Messenger/Messenger/UserPanel.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Messenger
+{
+    /// <summary>
+    /// Логика взаимодействия для UserPanel.xaml
+    /// </summary>
+    public partial class UserPanel : UserControl
+    {   
+        public string Username
+        {
+            get { return (string)GetValue(UsernameProperty); }
+            set { SetValue(UsernameProperty, value); }
+        }
+        public static readonly DependencyProperty UsernameProperty =
+            DependencyProperty.Register("Username", typeof(string),
+            typeof(UserPanel), new UIPropertyMetadata("", new PropertyChangedCallback(UsernameChanged)));
+        
+        private static void UsernameChanged(DependencyObject depObj, DependencyPropertyChangedEventArgs args)
+        {
+            ((UserPanel)depObj).tbUsername.Text = $" {args.NewValue.ToString()}";
+        }
+
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value);}
+        }
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof(string),
+            typeof(UserPanel), new UIPropertyMetadata("", new PropertyChangedCallback(MessageChanged)));
+        private static void MessageChanged(DependencyObject depObj, DependencyPropertyChangedEventArgs args)
+        {
+            ((UserPanel)depObj).tbMessage.Text = $" {args.NewValue.ToString()}";
+        }
+
+        public string Time
+        {
+            get { return (string)GetValue(TimeProperty); }
+            set { SetValue(TimeProperty, value); tbTime.Text = value; }
+        }
+        public static readonly DependencyProperty TimeProperty =
+            DependencyProperty.Register("Time", typeof(string),
+            typeof(UserPanel), new UIPropertyMetadata("", new PropertyChangedCallback(TimeChanged)));
+        private static void TimeChanged(DependencyObject depObj, DependencyPropertyChangedEventArgs args)
+        {
+            ((UserPanel)depObj).tbTime.Text = args.NewValue.ToString();
+        }
+
+        public string Avatar
+        {
+            get { return (string)GetValue(AvatarProperty); }
+            set { SetValue(AvatarProperty, value); }
+        }
+        public static readonly DependencyProperty AvatarProperty =
+            DependencyProperty.Register("Avatar", typeof(string),
+            typeof(UserPanel), new UIPropertyMetadata("", new PropertyChangedCallback(AvatarChanged)));
+        private static void AvatarChanged(DependencyObject depObj, DependencyPropertyChangedEventArgs args)
+        {
+            ((UserPanel)depObj).ibAvatar.ImageSource = new BitmapImage(new Uri(args.NewValue.ToString(), UriKind.Relative));
+        }
+
+        public UserPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
